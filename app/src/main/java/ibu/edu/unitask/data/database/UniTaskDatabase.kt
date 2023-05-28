3c33656d4dcf88284d0af43120f63d7a7e2b7fd9
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-05-28 16:47:55 +0200

    small error

--- app/src/main/java/ibu/edu/unitask/data/database/UniTaskDatabase.kt
+++ app/src/main/java/ibu/edu/unitask/data/database/UniTaskDatabase.kt
@@ -14,7 +14,7 @@ import ibu.edu.unitask.data.models.Task
 @TypeConverters(value = [DateConverter::class])
 @Database(
     entities = [Task::class],
-    version = 1,git
+    version = 1,
     exportSchema = false
 )
 abstract class UniTaskDatabase : RoomDatabase(){
