ec1dd8cbf586bb043af7fd3e7643d73f76f5f9b8
Dino Krso <dino.krso45@hotmail.com>
2023-06-01 12:48:47 +0200

    Zadnji update

--- app/src/main/java/ibu/edu/unitask/ui/details/TaskDetailsScreen.kt
+++ app/src/main/java/ibu/edu/unitask/ui/details/TaskDetailsScreen.kt
@@ -1,57 +1,52 @@
 package ibu.edu.unitask.ui.details
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.ClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import ibu.edu.unitask.R
-import ibu.edu.unitask.data.models.Task
 import ibu.edu.unitask.ui.navigation.NavigationDestination
 import ibu.edu.unitask.ui.navigation.UniTaskTopAppBar
 import ibu.edu.unitask.ui.utils.DateFormatter
-import java.util.Calendar
 import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.text.font.FontWeight
 
 
 object TaskDetailsDestination : NavigationDestination{
@@ -60,7 +55,7 @@ object TaskDetailsDestination : NavigationDestination{
     override val icon = Icons.Default.ArrowForward
 }
 
-@SuppressLint("ServiceCast")
+@SuppressLint("ServiceCast", "UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskDetailsScreen(
@@ -77,204 +72,143 @@ fun TaskDetailsScreen(
     LaunchedEffect(key1 = true) {
         viewModel.getTask(taskId)
     }
-    Box(
-        modifier = modifier
-            .fillMaxSize()
 
+    Scaffold(
+        topBar = {
+            UniTaskTopAppBar(
+                title = stringResource(TaskDetailsDestination.titleRes) + taskDetailsUiState.task.id.toString(),
+                canNavigateBack = canNavigateBack,
+                navigateUp = onNavigateBack
+            )
+        }
     ) {
-        Scaffold(
-            topBar = {
-                UniTaskTopAppBar(
-                    title = stringResource(TaskDetailsDestination.titleRes) + taskDetailsUiState.task.id.toString(),
-                    canNavigateBack = canNavigateBack,
-                    navigateUp = onNavigateBack
+        Column(
+            modifier = modifier
+                .background(Color(0xFFECE7EE))
+                .fillMaxSize()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Task Details",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(horizontal = 8.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(horizontal = 8.dp)
+            ) {
+                Checkbox(
+                    checked = taskDetailsUiState.task.isFinished,
+                    onCheckedChange = {}
+                )
+                Text(
+                    text = "Status: ",
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+                Text(
+                    text = if (taskDetailsUiState.task.isFinished) "Done" else "Not done",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier.padding(start = 4.dp)
                 )
             }
-        ) {
-            Column(
-                modifier = modifier
-                    .background(Color(0xFFECE7EE))
-                    .fillMaxSize()
-                    .padding(it)
-                    .padding(bottom = 50.dp),
 
-                horizontalAlignment = Alignment.Start,
-                verticalArrangement = Arrangement.Top
-            ) {
+            Spacer(modifier = Modifier.height(16.dp))
 
-                Row(
-                    modifier = modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Text(text = "Task Details",
-                        fontSize = 40.sp,
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                }
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    val status: String =
-                        if (taskDetailsUiState.task.isFinished) "Done" else "Not done"
-                    Checkbox(checked = taskDetailsUiState.task.isFinished, onCheckedChange = {})
-                    Text(text = "Status: ",
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                    Text(text = status,
-                        style = MaterialTheme.typography.headlineMedium
-                    )
-                }
+            InfoRow(
+                iconResId = R.drawable.baseline_info_24,
+                title = "Title: ",
+                content = taskDetailsUiState.task.title,
+                clipboardManager = clipboardManager
+            )
 
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = modifier.background(Color.Transparent)
-                ) {
-                    Icon(imageVector = Icons.Filled.Info, contentDescription = null)
-                    Text(
-                        text = "Title: ",
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                    Text(
-                        text = taskDetailsUiState.task.title,
-                    )
-                    Spacer(modifier = Modifier.weight(1f)) // Add a spacer to push the icon to the end
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_content_copy_24),
-                        contentDescription = "copy",
-                        modifier = Modifier
-                            .clickable {
-                                val annotatedString = buildAnnotatedString {
-                                    append(taskDetailsUiState.task.title)
-                                }
+            Spacer(modifier = Modifier.height(8.dp))
 
-                                clipboardManager.setText(annotatedString)
-                                Toast
-                                    .makeText(context, "Copied to clipboard", Toast.LENGTH_SHORT)
-                                    .show()
-                            }
-                            .padding(end = 10.dp)
-                            .padding(start = 8.dp)
-                            .align(Alignment.CenterVertically),
-                        tint = Color(0xFFFFFFF7)
-                    )
-                }
+            InfoRow(
+                iconResId = R.drawable.baseline_description_24,
+                title = "Description: ",
+                content = taskDetailsUiState.task.description,
+                clipboardManager = clipboardManager
+            )
 
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(painter = painterResource(id = R.drawable.baseline_description_24), contentDescription = null)
-                    Text(text = "Description: ",
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                    Text(
-                            text = taskDetailsUiState.task.description,
-                    )
-                    Spacer(modifier = Modifier.weight(1f)) // Add a spacer to push the icon to the end
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_content_copy_24),
-                        contentDescription = "copy",
-                        modifier = Modifier
-                            .clickable {
-                                val annotatedString = buildAnnotatedString {
-                                    append(taskDetailsUiState.task.description)
-                                }
+            Spacer(modifier = Modifier.height(8.dp))
 
-                                clipboardManager.setText(annotatedString)
-                                Toast
-                                    .makeText(context, "Copied to clipboard", Toast.LENGTH_SHORT)
-                                    .show()
-                            }
-                            .padding(end = 10.dp)
-                            .padding(start = 8.dp)
-                            .align(Alignment.CenterVertically),
-                        tint = Color(0xFFFFFFF7)
-                    )
-                }
+            InfoRow(
+                iconResId = R.drawable.baseline_subject_24,
+                title = "Course: ",
+                content = taskDetailsUiState.task.course,
+                clipboardManager = clipboardManager
+            )
 
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_subject_24),
-                        contentDescription = null
-                    )
-                    Text(text = "Course: ",
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                    Text(
-                        text = taskDetailsUiState.task.course,
-                    )
-                    Spacer(modifier = Modifier.weight(1f)) // Add a spacer to push the icon to the end
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_content_copy_24),
-                        contentDescription = "copy",
-                        modifier = Modifier
-                            .clickable {
-                                val annotatedString = buildAnnotatedString {
-                                    append(taskDetailsUiState.task.course)
-                                }
+            Spacer(modifier = Modifier.height(8.dp))
 
-                                clipboardManager.setText(annotatedString)
-                                Toast
-                                    .makeText(context, "Copied to clipboard", Toast.LENGTH_SHORT)
-                                    .show()
-                            }
-                            .padding(end = 10.dp)
-                            .padding(start = 8.dp)
-                            .align(Alignment.CenterVertically),
-                        tint = Color(0xFFFFFFF7)
-                    )
-                }
-
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(imageVector = Icons.Filled.DateRange, contentDescription = null)
-                    Text(text = "Due date: ",
-                        style = MaterialTheme.typography.bodyLarge)
-                    Text(
-                        text = DateFormatter(taskDetailsUiState.task.dueDate),
-                    )
-                    Spacer(modifier = Modifier.weight(1f)) // Add a spacer to push the icon to the end
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_content_copy_24),
-                        contentDescription = "copy",
-                        modifier = Modifier
-                            .clickable {
-                                val annotatedString = buildAnnotatedString {
-                                    append(DateFormatter(taskDetailsUiState.task.dueDate))
-                                }
-
-                                clipboardManager.setText(annotatedString)
-                                Toast
-                                    .makeText(context, "Copied to clipboard", Toast.LENGTH_SHORT)
-                                    .show()
-                            }
-                            .padding(end = 10.dp)
-                            .padding(start = 8.dp)
-                            .align(Alignment.CenterVertically),
-                        tint = Color(0xFFFFFFF7)
-                    )
-                }
-
-
-            }
+            InfoRow(
+                iconResId = R.drawable.baseline_date_range_24,
+                title = "Due date: ",
+                content = DateFormatter(taskDetailsUiState.task.dueDate),
+                clipboardManager = clipboardManager
+            )
         }
     }
 }
-
-@Preview
 @Composable
-fun TaskDetailsPreview() {
-    val currentDate = Calendar.getInstance().time
-    val task = Task(
-        id = 5,
-        title = "Sample Task",
-        description = "This is a sample task description",
-        dueDate = currentDate,
-        course = "Maths",
-        isFinished = true
-    )
-}
+private fun InfoRow(
+    iconResId: Int,
+    title: String,
+    content: String,
+    clipboardManager: ClipboardManager
+) {
+    val context = LocalContext.current
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(horizontal = 8.dp)
+    ) {
+        Icon(
+            painter = painterResource(id = iconResId),
+            contentDescription = null,
+            modifier = Modifier.size(24.dp),
+            tint = Color(0xFF320064)
+        )
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+        Text(
+            text = content,
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.ExtraLight,
+            modifier = Modifier.padding(start = 4.dp)
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_content_copy_24),
+            contentDescription = "copy",
+            modifier = Modifier
+                .clickable {
+                    val annotatedString = buildAnnotatedString {
+                        append(content)
+                    }
 
+                    clipboardManager.setText(annotatedString)
+                    Toast
+                        .makeText(context, "Copied to clipboard", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                .padding(end = 10.dp)
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically),
+            tint = Color(0xFFFFFFF7)
+        )
+    }
+}
--- app/src/main/java/ibu/edu/unitask/ui/utils/DateTester.kt
+++ app/src/main/java/ibu/edu/unitask/ui/utils/DateTester.kt
@@ -4,5 +4,7 @@ import androidx.compose.ui.graphics.Color
 import java.util.Date
 
 fun DateTester(date1: Date, date2: Date): Color{
-    return if(date1 > date2) Color.White else Color(0xFFD11A2A)
+    val date1text = DateFormatter(date1)
+    val date2text = DateFormatter(date2)
+    return if(date1 > date2 || date1text.equals(date2text)) Color.White else Color(0xFFD11A2A)
 }
